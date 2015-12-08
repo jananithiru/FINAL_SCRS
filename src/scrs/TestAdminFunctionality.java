bac8249a26cbd5b2e9f7d339fa25ffe9ac64ebaf
Janani <janani.thiru@gmail.com>
2015-12-08 16:34:09 -0600

    Generated ant file

--- src/scrs/TestAdminFunctionality.java
+++ src/scrs/TestAdminFunctionality.java
@@ -10,9 +10,38 @@ import scrs.ShibbolethAuth.Token;
 
 public class TestAdminFunctionality {
 	// test admin add class into course and instructorandcourse table
+
+	@Test
+	public void TestAdminAddSameClassTwice() throws Exception {
+
+		SCRS testScrs = new SCRSImpl();
+		ShibbolethAuth sbAuth = new ShibbolethAuth();
+		Token tokenGenerator = sbAuth.tokenGenerator("John196", "password");
+		Token myTokenStu = ((SCRSImpl) testScrs).userLogin("YUWEI1005", "mypassword");
+
+		if (tokenGenerator != null) {
+
+			System.out.println("ADMIN ADD CLASS START");
+
+			assertEquals(false,
+					testScrs.adminAddClass(tokenGenerator, 888, "Advanced Database2", 1, 25, "Fall2015", 206,
+							"09/01/2014", "12/20/2014", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No",
+							"Databases", "CS"));
+			System.out.println("ADMIN ADD CLASS START");
+
+			assertEquals(false,
+					testScrs.adminAddClass(tokenGenerator, 888, "Advanced Database2", 1, 25, "Fall2015", 206,
+							"09/01/2014", "12/20/2014", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No",
+							"Databases", "CS"));
+
+		
+		}
+
+	}
+
 	@Test
 	public void TestAdminAddClass() throws Exception {
-		
+
 		SCRS testScrs = new SCRSImpl();
 		ShibbolethAuth sbAuth = new ShibbolethAuth();
 		Token tokenGenerator = sbAuth.tokenGenerator("John196", "password");
@@ -31,9 +60,8 @@ public class TestAdminFunctionality {
 							"09/01/2014", "12/20/2014", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No",
 							"Databases", "CS"));
 			assertEquals(false,
-					testScrs.adminAddClass(myTokenStu, 666, "Advanced Database1", 2, 25, "Fall2015", 206,
-							"09/01/2014", "12/20/2014", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No",
-							"Databases", "CS"));
+					testScrs.adminAddClass(myTokenStu, 666, "Advanced Database1", 2, 25, "Fall2015", 206, "09/01/2014",
+							"12/20/2014", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No", "Databases", "CS"));
 			assertEquals(false,
 					testScrs.adminAddClass(tokenGenerator, 555, "Advanced Database1", -1, 25, "Fall2015", 206,
 							"09/01/2014", "12/20/2014", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No",
@@ -68,9 +96,8 @@ public class TestAdminFunctionality {
 
 			System.out.println("ADMIN EDIT CLASS START");
 
-			assertEquals(true,
-					testScrs.adminEditClass(tokenGenerator, 888, "Advanced Database3", 3, 206, "09/01/2014",
-							"09/01/2015", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No", "Databases", "CS"));
+			assertEquals(true, testScrs.adminEditClass(tokenGenerator, 888, "Advanced Database3", 3, 206, "09/01/2014",
+					"09/01/2015", "9:00", "10:30", "Tu,Th", "KHKH110", "Lecture", "No", "Databases", "CS"));
 
 		}
 	}
